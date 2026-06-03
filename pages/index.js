f5954ff7dab6d86dd2cb2248fc3f4d7351921471
luigizanella <luigizanella4@gmail.com>
2026-06-03 02:42:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -244,7 +244,7 @@ export default function Home() {
           </div>
         </main>
 
-        <footer className="mt-8 pt-6 pb-4 border-t border-gray-200 dark:border-gray-800 text-center">
+        <footer className="mt-8 py-9 border-t border-gray-200 dark:border-gray-800 text-center">
           <p className="text-xs text-gray-400">ZanellaSpeed · Powered by Google PageSpeed Insights · Feito por Luigi Zanella</p>
         </footer>
       </div>
