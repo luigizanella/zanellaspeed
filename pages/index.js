f14b23c4d1de4cb99c5745a674aac4729358c52a
luigizanella <luigizanella4@gmail.com>
2026-06-03 02:59:19 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -234,7 +234,7 @@ export default function Home() {
                 </div>
               </div>
             ) : (
-              <div className="text-center py-12 text-gray-400 dark:text-gray-600">
+              <div className="text-center py-32 text-gray-400 dark:text-gray-600">
                 <svg className="w-12 h-12 mx-auto mb-3 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
                 </svg>
